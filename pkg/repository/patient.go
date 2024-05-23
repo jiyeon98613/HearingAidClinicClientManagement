53813a268aeb5d4b3687aa54b8deac8d138eed87
Jiyeon Kim <jkim9510@usc.edu>
2024-05-22 19:19:51 -0700

    Update patient.go

--- pkg/repository/patient.go
+++ pkg/repository/patient.go
@@ -5,15 +5,23 @@ import (
     "HearingAidClinicClientManagement/pkg/model"
 )
 
-type PatientRepository struct {
+// PatientRepository describes the interface for patient storage operations
+type PatientRepository interface {
+    Save(patient *model.Patient) error
+}
+
+// MySQLPatientRepository is an implementation of PatientRepository using a MySQL database
+type MySQLPatientRepository struct {
     db *sql.DB
 }
 
-func NewPatientRepository(db *sql.DB) *PatientRepository {
-    return &PatientRepository{db: db}
+// NewMySQLPatientRepository creates a new instance of MySQLPatientRepository
+func NewMySQLPatientRepository(db *sql.DB) *MySQLPatientRepository {
+    return &MySQLPatientRepository{db: db}
 }
 
-func (r *PatientRepository) Save(patient *model.Patient) error {
-    _, err := r.db.Exec("INSERT INTO patients (id, name, age) VALUES ($1, $2, $3)", patient.ID, patient.Name, patient.Age)
+// Save inserts a new patient record into the database
+func (r *MySQLPatientRepository) Save(patient *model.Patient) error {
+    _, err := r.db.Exec("INSERT INTO patients (id, name, age) VALUES (?, ?, ?)", patient.ID, patient.Name, patient.Age)
     return err
 }
