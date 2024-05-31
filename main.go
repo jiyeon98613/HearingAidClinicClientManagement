5c1cb95eee90f440d82ecc98234f51ee4d6e984c
JiyeonKim <jkim9510@usc.edu>
2024-05-31 16:11:48 -0700

    changed

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 	"log"
 	"net/http"
 
-	"workspace/src/github.com/jiyeon98613/HearingAidClinicClientManagement/pkg/bootstrap"
-	"workspace/src/github.com/jiyeon98613/HearingAidClinicClientManagement/pkg/model"
-	"workspace/src/github.com/jiyeon98613/HearingAidClinicClientManagement/pkg/webhook"
+	"github.com/jiyeon98613/HearingAidClinicClientManagement/pkg/bootstrap"
+	"github.com/jiyeon98613/HearingAidClinicClientManagement/pkg/model"
+	"github.com/jiyeon98613/HearingAidClinicClientManagement/pkg/webhook"
 )
 
 func main() {
